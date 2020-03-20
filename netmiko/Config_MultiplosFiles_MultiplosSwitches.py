99d534e066123bce33b631258b15cec4bb475436
porfiriothiagoroot <62341224+porfiriothiagoroot@users.noreply.github.com>
2020-03-20 15:58:15 -0300

    Script utilizando múltiplos arquivos de suporte
    Esse script foi criado para atribuir diversas configurações para diversos switches via dois ou mais arquivos de suporte.

---
+++ netmiko/Config_MultiplosFiles_MultiplosSwitches.py
@@ -0,0 +1,66 @@
+# O arquivo iosv_l2_access está sendo setado uma configuração que irá ser replicado para os devices iosv_l2_s5, iosv_l2_s4, iosv_l2_s3
+# O arquivo iosv_l2_core está sendo setado uma configuração que irá ser replicado para os devices iosv_l2_s2, iosv_l2_s1
+
+
+from netmiko import ConnectHandler
+
+iosv_l2_s1 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.71',
+    'username': 'thiago',
+    'password': 'thiago',
+}
+
+iosv_l2_s2 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.72',
+    'username': 'thiago',
+    'password': 'thiago',
+}
+
+iosv_l2_s3 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.73',
+    'username': 'thiago',
+    'password': 'thiago',
+}
+
+iosv_l2_s4 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.74',
+    'username': 'thiago',
+    'password': 'thiago',
+}
+
+iosv_l2_s5 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.75',
+    'username': 'thiago',
+    'password': 'thiago',
+}
+
+
+with open('iosv_l2_access') as f:
+    lines = f.read().splitlines()
+print lines
+
+
+all_devices = [iosv_l2_s5, iosv_l2_s4, iosv_l2_s3]
+
+for devices in all_devices:
+    net_connect = ConnectHandler(**devices)
+    output = net_connect.send_config_set(lines)
+    print output 
+
+
+with open('iosv_l2_core') as f:
+    lines = f.read().splitlines()
+print lines
+
+
+all_devices = [iosv_l2_s2, iosv_l2_s1]
+
+for devices in all_devices:
+    net_connect = ConnectHandler(**devices)
+    output = net_connect.send_config_set(lines)
+    print output 
